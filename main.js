6739d6574c10b38844e9539b4e2c8dbd8d26aff5
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-08-24 17:45:41 +0530

    2nd removing users

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let nameInput = document.querySelector('[name="fullname"]');
 let emailInput = document.querySelector('[name="email"]');
 let phoneInput = document.querySelector('[name="phone"]');
 let userList = document.getElementById('userlist');
+
 form.addEventListener('submit',function(e){
     e.preventDefault();
     
@@ -10,13 +11,30 @@ form.addEventListener('submit',function(e){
     let email = emailInput.value;
     let phone = phoneInput.value;
 
-    let newUser = {name,email,phone};
-    localStorage.setItem("users",JSON.stringify(newUser));
+    const newUser = {name,email,phone};
+    const users = JSON.stringify(newUser);
+    localStorage.setItem("user",users);
 
     let listItem  = document.createElement('li');
-    listItem.textContent = `${name} ${email} ${phone}`;
+    listItem.textContent = `${name} - ${email} - ${phone}`;
     userList.appendChild(listItem);
 
     nameInput.value= "";
     emailInput.value="";
+
+    let delBtn = document.createElement('button');
+    delBtn.className = 'delete';
+
+    delBtn.appendChild(document.createTextNode('delete'));
+    listItem.appendChild(delBtn);
+})
+
+userList.addEventListener('click',function(e){
+    if(e.target.classList.contains('delete'))
+    {
+        let li = e.target.parentElement;
+        userList.removeChild(li);
+        localStorage.removeItem("user");
+     }
+
 })
